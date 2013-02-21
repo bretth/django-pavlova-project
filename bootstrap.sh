b2147d037244069cbb7c650c3b64ad5f3002764e
Brett Haydon <brett@haydon.id.au>
2013-02-21 13:32:37 +1100

    typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 pip install django
-django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json .
+django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json $1 .
 pip install -r requirements/dev.txt
 python manage.py syncdb --noinput
