1b12c3d310906e3a302704e6acb647880355f0e6
Brett Haydon <brett@haydon.id.au>
2013-02-21 13:28:10 +1100

    Change the context to the cwd

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 pip install django
-django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json $1
-pip install -r $1/requirements/dev.txt
-python $1/manage.py syncdb --noinput
+django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json .
+pip install -r requirements/dev.txt
+python manage.py syncdb --noinput
