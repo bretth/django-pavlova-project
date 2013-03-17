f08a9cb7fa3706558f146037570227b4746fac60
Brett Haydon <brett@haydon.id.au>
2013-03-17 19:13:00 +1100

    Remove redundant path append.

--- project/project_name/settings/base.py
+++ project/project_name/settings/base.py
@@ -11,11 +11,3 @@ SITE_ROOT = dirname(DJANGO_ROOT)
 # Site name:
 SITE_NAME = basename(DJANGO_ROOT)
 
-# Add our project to our pythonpath, this way we don't need to type our project
-# name in our dotted import paths:
-path.append(DJANGO_ROOT)
-
-
-
-
-
