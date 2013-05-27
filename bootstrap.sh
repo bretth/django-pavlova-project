6b9c1155e1ded1652ad8161a34d73923600e35f1
Brett Haydon <brett@haydon.id.au>
2013-05-27 22:16:06 +1000

    Better bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,41 @@
-if [ ! $# == 2 ]; then
-    if [ $2 == dev ]; then
-        pip install -e git+git://github.com/django/django.git#egg=django
-    fi
-else
-    pip install django
+#!/bin/sh
+function help {
+echo "Usage: ./bootstrap.sh <project_name> [--dev]"
+}
 
+PR="==> "
+
+DJANGO="pip install django"
+if [ $# -eq 2 ]; then
+        if [ $2 == --dev ]; then
+                DJANGO="pip install -e git+git://github.com/django/django.git#egg=django"
+        else
+                echo "Invalid argument"
+                help
+                exit 1
+        fi
 fi
-django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json,cfg $1 .
-pip install -r requirements/dev.txt
-chmod +x manage.py
-ln -s djangoproject/djangoproject/templates templates
-python manage.py syncdb --noinput
+if [ $# -eq 0 -o $# -gt 2 ]; then 
+        help
+        exit
+fi
+
+echo $PR$DJANGO
+$DJANGO
+echo
+SP="django-admin.py startproject --template=https://github.com/bretth/django-pavlova-project/zipball/master --extension=py,rst,html,json,cfg $1 ."
+echo $PR$SP
+$SP
+echo
+REQ="pip install -r requirements/dev.txt"
+echo $PR$REQ
+$REQ
+echo
+SYNC="python manage.py syncdb --noinput"
+echo $PR$SYNC
+$SYNC
+echo
+CH="chmod +x manage.py"
+echo $PR$CH
+$CH
+
